e4ab8b863232b3ae0984b1abe5f385bb0f435c27
Hanze-C <hanze2010@outlook.com>
2025-01-11 19:56:11 +0800

    upd

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Gamepad, Book, Copyright, Fire, LaptopCode, TelegramPlane } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Calculator, CommentRegular, SyncAlt, Compass, Gamepad, Book, PaintBrush, CopyrightRegular, Fire, LaptopCode, TelegramPlane, Sitemap } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -65,8 +65,13 @@ const siteLinksList = computed(() => {
 const siteIcon = {
   Blog,
   Cloud,
-  Copyright,
+  CopyrightRegular,
+  CommentRegular,
+  SyncAlt,
   CompactDisc,
+  Sitemap,
+  Calculator,
+  PaintBrush,
   Compass,
   Book,
   Fire,
