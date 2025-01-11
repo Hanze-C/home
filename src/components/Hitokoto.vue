c02d74fb5a84bba715c6aa9a260d867d671aac0d
Hanze-C <hanze2010@outlook.com>
2025-01-11 20:29:53 +0800

    Update Hitokoto.vue

--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -115,6 +115,7 @@ onMounted(() => {
       overflow: hidden;
       display: -webkit-box;
       -webkit-line-clamp: 3;
+      line-clamp: 3;
       -webkit-box-orient: vertical;
     }
     .from {
